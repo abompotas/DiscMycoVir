45bcfc4aa22e14eaf76b39a44ea0b4deba985542
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2024-05-30 17:01:12 +0300

    fixing fastqc_analysis error

--- analyser/lib/analysis.sh
+++ analyser/lib/analysis.sh
@@ -75,11 +75,11 @@ cd "/tmp/${sample_name}"
 tmp=(${forward_file//./ })
 fastqc_out_f="${tmp[0]}_fastqc"
 if [ "${single_paired}" = "single" ]; then
-  fastqc -o . fastqc_analysis "${forward_file}" -t $threads -q
+  fastqc -o ./fastqc_analysis "${forward_file}" -t $threads -q
 else
   tmp=(${reverse_file//./ })
   fastqc_out_r="${tmp[0]}_fastqc"
-  fastqc -o . fastqc_analysis "${forward_file}" "${reverse_file}" -t $threads -q
+  fastqc -o ./fastqc_analysis "${forward_file}" "${reverse_file}" -t $threads -q
 fi
 
 
@@ -87,4 +87,4 @@ fi
 mkdir -p "${output_dir}"
 mv fastqc_analysis "$output_dir"
 cd "${current_dir}"
-#rm -r "/tmp/${sample_name}"
+rm -r "/tmp/${sample_name}"
