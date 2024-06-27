4090fcd9191376ddc10dced412178002cf0391ea
Agorakis Bompotas <mpompotas@ceid.upatras.gr>
2024-06-27 20:01:59 +0300

    Bug fix for BLAST queue

--- analyser/process_queue_discovery_blast.py
+++ analyser/process_queue_discovery_blast.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         for job in jobs_batch:
             discvir.mark_as_started_blast(job['id'])
             cwd = os.path.dirname(os.path.abspath(__file__))
-            discovery_exec = os.path.join(cwd, 'lib/discovery.sh')
+            discovery_exec = os.path.join(cwd, 'lib/discovery_blast.sh')
             discovery_dir = os.path.join(config['args']['output'], str(job['id']), 'discovery')
             Popen([discovery_exec, '-d', discovery_dir])
 
@@ -27,7 +27,7 @@ if __name__ == '__main__':
             discovery_flag = os.path.join(config['args']['output'], str(job['id']), 'discovery', 'blast_finished')
             if os.path.exists(discovery_flag):
                 timestamp = discvir.mark_as_completed_discovery(job['id'])
-            if not notify_user(config, job['user'], job['id'], timestamp, 'discovery'):
-                print('[{}|{}]: Email (discovery) not sent to {}'.format(job['id'], job['sample_name'], job['user']))
+                if not notify_user(config, job['user'], job['id'], timestamp, 'discovery'):
+                    print('[{}|{}]: Email (discovery) not sent to {}'.format(job['id'], job['sample_name'], job['user']))
 
         sleep(config['queue']['sleep'])
